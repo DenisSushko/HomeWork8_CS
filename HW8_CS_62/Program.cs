30f25f0575a2ca0188e38d8d66307ef35f775b46
Denis <help@denis-getcourse.ru>
2022-11-24 18:51:21 +0300

    HW8_CS_62

---
+++ HW8_CS_62/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4. 
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] createSpirale()
+{
+    int [,] arr = new int[4,4];
+    int num = 1;
+    int i = 0;
+    int j = 0;
+    int size = 4;
+
+    while (num <= size * size)
+    {
+        arr[i, j] = num;
+        if (i <= j + 1 && i + j < size - 1)
+            j++;
+        else if (i < j && i + j >= size - 1)
+            i++;
+        else if (i >= j && i + j > size - 1)
+            j--;
+        else
+            i--;
+        num++;
+    }
+    return arr;
+}
+
+void printArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            if (arr[i, j] < 10)
+            {
+                Console.Write("0" + arr[i, j]);
+                Console.Write(" ");
+            }
+            else 
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+printArray(createSpirale());
